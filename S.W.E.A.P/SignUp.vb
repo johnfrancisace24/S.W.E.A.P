cb9d892af2508db48f68ac5d0c23ee111dee63f2
benhur <binverwin16@gmail.com>
2023-06-01 10:28:12 +0800

    change

--- S.W.E.A.P/SignUp.vb
+++ S.W.E.A.P/SignUp.vb
@@ -2,42 +2,60 @@
 Public Class SignUp
     Dim conn As New MySqlConnection("server=172.30.206.156;port=3306;username=sweapp;password=druguser;database=sweap")
     Dim rid As MySqlDataReader
+    Dim error_msg(13) As String
+    Dim random As Integer = 0
+    '-------FUNCTIONS--------------------------------------------------------
+    Public Sub valid_clear(field, name)
+        If field = "" Then
+            error_msg(random) = name & " can't be blank."
+            random = random + 1
+        End If
+    End Sub
+    '------------------------------------------------------------------------
     Private Sub SignUp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         rdiobttnPer.Checked = True
     End Sub
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-
-        Try
-            conn.Open()
-            Dim cmd As New MySqlCommand("insert into users(username, password, first_name, middle_name, last_name, position, image, is_admin, created_at, updated_at)values(@UNAME, @PW, @FNAME, @MNAME, @LNAME, @POS, @IMG, 0, now(), now());
-                                         insert into user_info(user_id, address, email, educational, birthdate, office, employment_status, committee)values(last_insert_id(), @ADRS, @EMAIL, @EDUC, @BDAY, @OFFICE, @EMTYPE, @COMM);
+        valid_clear(txtbxUser.Text, "Name")
+        If error_msg.Length = 0 Then
+            Try
+                conn.Open()
+                Dim cmd As New MySqlCommand("insert into users(username, password, first_name, middle_name, last_name, position, image, is_admin, created_at, updated_at)values(@UNAME, @PW, @FNAME, @MNAME, @LNAME, @POS, @IMG, 0, now(), now());
+                                         insert into user_info(user_id, address, contact, email, educational, birthdate, office, employment_status, committee)values(last_insert_id(), @ADRS, @CONTACT, @EMAIL, @EDUC, @BDAY, @OFFICE, @EMTYPE, @COMM);
                                          insert into beneficiaries(user_id, full_name, relationship, age)values((select id from users where username=@HOOK), @BNAME1, @BREL1, @BAGE1);", conn)
-            cmd.Parameters.AddWithValue("@UNAME", txtbxUser.Text)
-            cmd.Parameters.AddWithValue("@PW", txtbxPass.Text)
-            cmd.Parameters.AddWithValue("@FNAME", txtbxFname.Text)
-            cmd.Parameters.AddWithValue("@MNAME", txtbxMname.Text)
-            cmd.Parameters.AddWithValue("@LNAME", txtbxLname.Text)
-            cmd.Parameters.AddWithValue("@POS", comboPos.SelectedItem)
-            cmd.Parameters.AddWithValue("@IMG", "Image.jpg")
-            cmd.Parameters.AddWithValue("@ADRS", txtbxAddrs.Text)
-            cmd.Parameters.AddWithValue("@EMAIL", txtbxEmail.Text)
-            cmd.Parameters.AddWithValue("@EDUC", txtbxEducAt.Text)
-            cmd.Parameters.AddWithValue("@BDAY", pickBday.Value)
-            cmd.Parameters.AddWithValue("@OFFICE", comboOffice.SelectedItem)
-            cmd.Parameters.AddWithValue("@EMTYPE", comboEmployStat.SelectedItem)
-            cmd.Parameters.AddWithValue("@COMM", comboCommit.SelectedItem)
-            cmd.Parameters.AddWithValue("@BNAME1", txtbxBF1.Text)
-            cmd.Parameters.AddWithValue("@BREL1", txtbxBR1.Text)
-            cmd.Parameters.AddWithValue("@BAGE1", txtbxBA1.Text)
-            cmd.Parameters.AddWithValue("@HOOK", txtbxUser.Text)
-            cmd.ExecuteNonQuery()
-            MsgBox("napasok na")
-        Catch ex As Exception
-            MsgBox("doesnt work lmao")
-        Finally
-            conn.Close()
-        End Try
+                cmd.Parameters.AddWithValue("@UNAME", txtbxUser.Text)
+                cmd.Parameters.AddWithValue("@PW", txtbxPass.Text)
+                cmd.Parameters.AddWithValue("@FNAME", txtbxFname.Text)
+                cmd.Parameters.AddWithValue("@MNAME", txtbxMname.Text)
+                cmd.Parameters.AddWithValue("@LNAME", txtbxLname.Text)
+                cmd.Parameters.AddWithValue("@POS", comboPos.SelectedItem)
+                cmd.Parameters.AddWithValue("@IMG", "Image.jpg")
+                cmd.Parameters.AddWithValue("@ADRS", txtbxAddrs.Text)
+                cmd.Parameters.AddWithValue("@CONTACT", txtbxCntct.Text)
+                cmd.Parameters.AddWithValue("@EMAIL", txtbxEmail.Text)
+                cmd.Parameters.AddWithValue("@EDUC", txtbxEducAt.Text)
+                cmd.Parameters.AddWithValue("@BDAY", pickBday.Value)
+                cmd.Parameters.AddWithValue("@OFFICE", comboOffice.SelectedItem)
+                cmd.Parameters.AddWithValue("@EMTYPE", comboEmployStat.SelectedItem)
+                cmd.Parameters.AddWithValue("@COMM", comboCommit.SelectedItem)
+                cmd.Parameters.AddWithValue("@BNAME1", txtbxBF1.Text)
+                cmd.Parameters.AddWithValue("@BREL1", txtbxBR1.Text)
+                cmd.Parameters.AddWithValue("@BAGE1", txtbxBA1.Text)
+                cmd.Parameters.AddWithValue("@HOOK", txtbxUser.Text)
+                cmd.ExecuteNonQuery()
+                MsgBox("napasok na")
+            Catch ex As Exception
+                MsgBox("doesnt work lmao")
+            Finally
+                conn.Close()
+            End Try
+        Else
+            MsgBox(error_msg(0))
+
+
+        End If
+
     End Sub
 
 
