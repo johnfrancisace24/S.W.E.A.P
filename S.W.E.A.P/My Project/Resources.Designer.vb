19bc886ffa3ddb97626708bf06295beff1190c95
ACER TRAVELMATE <ACER TRAVELMATE@DSWD>
2023-06-07 15:29:25 +0800

    #

--- S.W.E.A.P/My Project/Resources.Designer.vb
+++ S.W.E.A.P/My Project/Resources.Designer.vb
@@ -193,15 +193,19 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-
-        Friend ReadOnly Property output_onlinegiftools_min() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("output-onlinegiftools-min", resourceCulture)
-
         Friend ReadOnly Property ka() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("ka", resourceCulture)
-
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property output_onlinegiftools_min() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("output-onlinegiftools-min", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
