5fd48acf562dc378dc9e797d18d4858c494ea476
angelobola <mr.mabola69@gmail.com>
2023-06-22 08:49:52 +0800

    1

--- S.W.E.A.P/signups.vb
+++ S.W.E.A.P/signups.vb
@@ -358,7 +358,4 @@ Public Class signups
         Guna2TabControl1.SelectedTab = TabPage2
     End Sub
 
-    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs) Handles Panel3.Paint
-
-    End Sub
 End Class
