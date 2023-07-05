e63f081da0558c2da8df3e34e9f661adab164407
ACER TRAVELMATE <ACER TRAVELMATE@DSWD>
2023-07-05 16:12:19 +0800

    #

--- S.W.E.A.P/admindash.vb
+++ S.W.E.A.P/admindash.vb
@@ -326,13 +326,13 @@ Public Class admindash
             cmd.ExecuteNonQuery()
             MsgBox("successfully updated.")
 
-            Guna2TabControl1.SelectedTab = personal
+            'Guna2TabControl1.SelectedTab = personal
         Catch ex As Exception
             MsgBox("Error:" & ex.Message)
         Finally
             conn.Close()
         End Try
-        pnlEmployee.Show()
+
         viewMembers("select users.id, concat(first_name, ' ', middle_name, ' ', last_name) as full_name, office, position, employment_status, 
                                             email from users left join user_info on users.id = user_info.user_id")
     End Sub
